4aea2701697d25bf488c52e31d4b922a4d426ef8
Seran Randiv <senanrandiu2018@gmail.com>
2022-07-12 07:10:16 +0530

    Footer components

---
+++ components/Footer/index.vue
@@ -0,0 +1,145 @@
+<template>
+  <div class="bg-[#3E3D3D]">
+    <div class="max-w-7xl m-auto py-[30px]">
+      <!-- link -->
+      <div class="text-[#FFFFFF] p-[20px] lg:flex lg:justify-between">
+        <!-- about us -->
+        <div class="lg:w-[300px]">
+          <p
+            class="text-[20px] font-bold text-center m-[20px] lg:text-start lg:my-[20px] lg:ml-[0] lg:mr-[20px]"
+          >
+            A LITTLE ABOUT US
+          </p>
+          <P
+            class="text-center font-normal mb-[20px] text-[#d3d3d3] lg:text-start lg:my-[20px] lg:ml-[0] lg:mr-[20px]"
+            >is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry's standard dummy text ever since
+            the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book.
+          </P>
+        </div>
+        <!-- resources -->
+        <div class="lg:flex lg:flex-col lg:items-start">
+          <p
+            class="text-[20px] font-bold text-center m-[20px] lg:text-start lg:my-[20px] lg:ml-[0] lg:mr-[20px]"
+          >
+            RESOURCES
+          </p>
+          <p class="text-center font-normal">
+            <a
+              class="hover:underline hover:underline-offset-1 text-[#d3d3d3] lg:text-start lg:my-[20px] lg:ml-[0] lg:mr-[20px]"
+              href="#"
+              >Sermons</a
+            >
+          </p>
+          <p class="flex justify-center text-center">
+            <a
+              class="hover:underline hover:underline-offset-1 text-[#d3d3d3] lg:text-start lg:ml-[0] lg:mr-[20px]"
+              href="#"
+              >Blog posts</a
+            >
+          </p>
+        </div>
+        <!-- help and support -->
+        <div class="lg:flex lg:flex-col lg:items-start">
+          <P
+            class="text-[20px] font-bold text-center m-[20px] lg:text-start lg:my-[20px] lg:ml-[0] lg:mr-[20px]"
+            >HELP & SUPPORT</P
+          >
+          <p class="text-center font-normal">
+            <a
+              class="hover:underline hover:underline-offset-1 text-[#d3d3d3] lg:text-start lg:my-[20px] lg:ml-[0] lg:mr-[20px]"
+              href="#"
+              >Contact Us</a
+            >
+          </p>
+          <P class="text-center font-normal"
+            ><a
+              class="hover:underline hover:underline-offset-1 text-[#d3d3d3] lg:text-start lg:my-[20px] lg:ml-[0] lg:mr-[20px]"
+              href="#"
+              >Knowledg Center</a
+            ></P
+          >
+        </div>
+        <!-- Follow us -->
+        <div class="lg:flex lg:flex-col lg:items-start">
+          <p
+            class="text-[20px] font-bold text-center m-[20px] lg:text-start lg:my-[20px] lg:ml-[0] lg:mr-[20px]"
+          >
+            FOLLOW US
+          </p>
+          <div class="flex items-center justify-center">
+            <!-- facebook icon -->
+            <svg
+              class="w-[35px] h-[35px] fill-[#3E3D3D] p-[5px] bg-white rounded-full mr-[10px] hover:bg-[#3E3D3D] hover:fill-white hover:border-2 transition duration-300 ease-in-out cursor-pointer"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 320 512"
+            >
+              <!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+              <path
+                d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"
+              />
+            </svg>
+            <!-- twitter icon -->
+            <svg
+              class="w-[35px] h-[35px] fill-[#3E3D3D] p-[5px] bg-white rounded-full mr-[10px] hover:bg-[#3E3D3D] hover:fill-white hover:border-2 transition duration-300 ease-in-out cursor-pointer"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+              <path
+                d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"
+              />
+            </svg>
+            <!-- youtube icon -->
+            <svg
+              class="w-[35px] h-[35px] fill-[#3E3D3D] p-[5px] bg-white rounded-full hover:bg-[#3E3D3D] hover:fill-white hover:border-2 transition duration-300 ease-in-out cursor-pointer"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 576 512"
+            >
+              <!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+              <path
+                d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"
+              />
+            </svg>
+          </div>
+        </div>
+        <!-- Leagal -->
+        <div class="lg:flex lg:flex-col lg:items-start">
+          <p
+            class="text-[20px] font-bold text-center m-[20px] lg:text-start lg:my-[20px] lg:ml-[0] lg:mr-[20px]"
+          >
+            LEAGAL
+          </p>
+          <P class="text-center font-normal"
+            ><a
+              class="hover:underline hover:underline-offset-1 text-[#d3d3d3] lg:text-start lg:my-[20px] lg:ml-[0] lg:mr-[20px]"
+              href="#"
+              >Privacy Policy</a
+            ></P
+          >
+          <p class="text-center font-normal">
+            <a
+              class="hover:underline hover:underline-offset-1 text-[#d3d3d3] lg:text-start lg:my-[20px] lg:ml-[0] lg:mr-[20px]"
+              href="#"
+              >Terms & Condition</a
+            >
+          </p>
+        </div>
+      </div>
+      <!-- copyright -->
+      <div>
+        <p
+          class="border-t-2 border-[#858585] py-[25px] text-center text-[#ffff]"
+        >
+          © 2010 - 2022 Sanctuary Fellowship. All Rights Reserved.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Footer",
+};
+</script>
