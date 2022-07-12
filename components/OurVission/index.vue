4b4d3ba0c430939cd16a2bea2f0209c8e0622a15
Seran Randiv <senanrandiu2018@gmail.com>
2022-07-12 07:15:09 +0530

    Our vission component

---
+++ components/OurVission/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="bg-dark_background py-14">
+    <div class="lg:max-w-7xl lg:m-auto lg:justify-center lg:flex lg:flex-row">
+      <div
+        class="text-center lg:text-left m-5 lg:flex lg:flex-col lg:justify-center lg:w-1/2"
+      >
+        <p class="text-[25px] lg:text-3xl font-bold text-white py-8">
+          Our Vision
+        </p>
+        <p class="text-[#D8D6D6]">
+          Psalm 149:6 Our vision as a church is to work toward building the
+          people of God on the foundation of the living word as commissioned by
+          God
+          <b><i> “go build my people”</i></b
+          >.
+          <br />
+          We desire to liberate people by the high praises offered unto God,
+          eventually bringing the people to a place of worship and intimacy in
+          the secret place of the most high. Our utmost calling is to make
+          people fall in love with Jesus, and to make them desire and love His
+          presence above all, thus making worship a lifestyle, so that they will
+          in turn go to the ends of the earth and make disciples out of
+          overflowing love and gratitude for the Lord, fulfilling the great
+          commission.
+        </p>
+      </div>
+      <div class="flex flex-col justify-center items-center">
+        <img
+          class="w-[456px] h-[663px] m-auto"
+          src="@/static/pastor.png"
+          alt="Pas.Ranil & sis.Devika"
+        />
+        <p class="text-[#D8D6D6] font-[20px]">
+          <i>Senior Pastor Ranil and Devika Seneratne</i>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "OurVission",
+};
+</script>
+<style></style>
