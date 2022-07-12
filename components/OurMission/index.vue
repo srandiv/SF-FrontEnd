ec170574f44c40803a7baaf791d2d1cb9b50f058
Seran Randiv <senanrandiu2018@gmail.com>
2022-07-12 07:14:34 +0530

    Our mission component

---
+++ components/OurMission/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="ourMission" class="lg:max-w-7xl flex m-auto">
+    <div class="lg:max-w-7xl text-center mx-5 my-10">
+      <p class="text-title_dark text-[25px] lg:text-3xl font-bold title py-8">
+        Our Mission
+      </p>
+      <p class="text-text_gray_light">
+        This church was birthed into being in 1987 with a little bible study
+        group that met on Thursday afternoons in a home. The group grew in
+        number and hunger when the Lord led us to start services on Sundays.
+        Today there are around 450 members, and three branches that meet on
+        Sundays with ministries for men, women, youth, teens and children.
+        Regular bible studies are conducted weekly which are recorded and
+        circulated among many hundreds of believers locally and abroad. All
+        glory to God "Ebenezer" who has brought us thus far carrying us in His
+        hands like little children. If not for His amazing grace we would not
+        have been able to come this far.
+      </p>
+      <p class="text-text_gray_light">
+        The land for the church was donated by a family, where a physical
+        structure (temporary) was erected in 1990 with six pillars and a tin
+        roof over us. This roof was never an obstacle for the presence of God
+        that continually over shadowed every meeting. The permanent structure
+        was completed in 1997 as God divinely and supernaturally provided every
+        requirement. "BELIEVE IT" we started the structure with less than one
+        fourth of the estimated cost with just prayer and total dependency on
+        God, with no fund raising projects. God’s provisions continued to flow
+        abundance financially, materially and hands that laboured in love until
+        the building was completed. The Lord did not stop at that but provided
+        us with sophisticated musical instruments, PA systems, multimedia
+        projectors and skilful musicians gifted and talented manpower that has
+        enabled us to launch this site to bless many of our own family of
+        believers that are in countries like Italy, America, Australia and
+        Canada.
+      </p>
+      <p class="text-text_gray_light mb-8">
+        We experienced the “First Outpouring of the Holy Spirit” in 1996 that
+        brought a revival and a cleansing of the church. During that season, the
+        Lord gave us Isaiah 60-The promise of Glory. We held on to it
+        faithfully. Thank God His wisdom is far greater, from a faithful stump
+        that was left behind the Lord brought forth an army of worshippers. We,
+        as generals created an atmosphere for the presence of God, and an
+        open-heaven for God to work. The glory of God descended upon us
+        exploding the atmosphere. People became hungry for God’s glory. We saw
+        and experienced His awesome glory literally.
+      </p>
+      <p class="text-title_dark font-bold text-[20px]">
+        <i>
+          “Like Moses, our hearts cry out as a church "Lord show us Your
+          Glory"</i
+        >
+      </p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "OurMission",
+};
+</script>
+<style></style>
