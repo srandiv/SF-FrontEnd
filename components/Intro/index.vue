5da2777e248441bd5044a12668cff45cb3a75b67
Seran Randiv <senanrandiu2018@gmail.com>
2022-07-12 07:11:25 +0530

    Intro components index

---
+++ components/Intro/index.vue
@@ -0,0 +1,19 @@
+<script setup>
+import NavBar from "../NavBar";
+import IntroContent from "./IntroContent";
+</script>
+<template>
+  <div
+    class="my-0 bg-[url('@/static/background.jpg')] bg-cover h-3/4 lg:h-screen"
+  >
+    <NavBar />
+    <div class="flex flex-col items-center m-auto h-screen">
+      <IntroContent />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Intro",
+};
+</script>
