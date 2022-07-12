0c40d8b90ab903afd457c4c56d80967d425018a4
Seran Randiv <senanrandiu2018@gmail.com>
2022-07-12 07:19:32 +0530

    Added main components to home page

---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<script setup>
+import "animate.css";
+import Intro from "../components/Intro";
+import OurMission from "../components/OurMission";
+import Ourvission from "../components/OurVission";
+import Live from "../components/Live";
+import Welcome from "../components/Welcome";
+import Community from "../components/Community";
+import UpcomingEvent from "../components/UpcomingEvents";
+import LatestSermons from "../components/LatestSermons";
+import LateastPost from "../components/LateastPost";
+import ContactUs from "../components/ContactUs";
+import NewsLatter from "../components/NewsLetter";
+import Footer from "../components/Footer";
+</script>
+<template>
+  <div>
+    <Intro />
+    <Live />
+    <Welcome />
+    <OurVission />
+    <OurMission />
+    <Community />
+    <UpcomingEvents />
+    <LatestSermons />
+    <LateastPost />
+    <ContactUs />
+    <NewsLetter />
+    <Footer />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
