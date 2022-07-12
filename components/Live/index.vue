b368b44c6ae2314898265e39d9a65d107a2f08d3
Seran Randiv <senanrandiu2018@gmail.com>
2022-07-12 07:13:37 +0530

    Live streame component

---
+++ components/Live/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div
+    class="max-w-screen bg-gray-100 m-auto flex flex-col justify-center items-center pb-14"
+  >
+    <h2
+      class="text-[25px] text-title_dark lg:text-3xl font-bold title pt-12 pb-12"
+    >
+      Live Stream
+    </h2>
+
+    <!-- Youtube embeded Link -->
+    <!-- This Element has a custom css -->
+    <div class="m-auto">
+      <div class="flex justify-center">
+        <iframe
+          class=""
+          src="https://www.youtube.com/embed/Hh4QEcksgNQ"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </div>
+    <!-- Youtube embeded Link end -->
+  </div>
+</template>
+<script>
+export default {
+  name: "Live",
+};
+</script>
+
+<style scoped>
+iframe {
+  width: 300px;
+  height: 150px;
+  border-radius: 10px;
+}
+
+@media only screen and (min-width: 600px) {
+  iframe {
+    width: 600px;
+    height: 300px;
+  }
+}
+
+@media only screen and (min-width: 1024px) {
+  iframe {
+    width: 900px;
+    height: 450px;
+  }
+}
+</style>
