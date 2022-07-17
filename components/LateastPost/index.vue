6498ce22c548f52210ceaf24eb00de9a6fc0aa84
Seran Randiv <senanrandiu2018@gmail.com>
2022-07-17 23:14:13 +0530

    Added nuxt link to read more btn

--- components/LateastPost/index.vue
+++ components/LateastPost/index.vue
@@ -66,17 +66,23 @@ import VueSlickCarousel from "vue-slick-carousel";
         <button
           class="btn text-[#565656] text-[18px] font-medium bg-white py-[10px] px-[25px] rounded-full drop-shadow-[0px_5px_15px_rgba(0, 0, 0, 0.35)] hover:text-[20px] ease-out duration-300"
         >
-          <a href="#" class="flex flex-row items-center justify-center"
-            >Read More
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 256 512"
-              class="w-[20px] h-[20px] fill-[#565656]"
-            >
-              <path
-                d="M64 448c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L178.8 256L41.38 118.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160c12.5 12.5 12.5 32.75 0 45.25l-160 160C80.38 444.9 72.19 448 64 448z"
-              /></svg
-          ></a>
+          <a href="#" class="flex flex-row items-center justify-center">
+            <nuxt-link
+              to="/blog"
+              class="flex flex-row items-center justify-center"
+              >Events Calender
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 256 512"
+                class="w-[20px] h-[20px] fill-[#565656]"
+              >
+                <path
+                  d="M64 448c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L178.8 256L41.38 118.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160c12.5 12.5 12.5 32.75 0 45.25l-160 160C80.38 444.9 72.19 448 64 448z"
+                />
+              </svg>
+            </nuxt-link>
+          </a>
         </button>
       </div>
       <!-- Read more button end-->
