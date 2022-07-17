b1c1812f020245c706bd475a8d9f52ad1303102c
Seran Randiv <senanrandiu2018@gmail.com>
2022-07-18 00:02:37 +0530

    Added id

--- components/Live/index.vue
+++ components/Live/index.vue
@@ -1,10 +1,6 @@
 <template>
-  <div
-    class="max-w-screen bg-gray-100 m-auto flex flex-col justify-center items-center pb-14"
-  >
-    <h2
-      class="text-[25px] text-title_dark lg:text-3xl font-bold title pt-12 pb-12"
-    >
+  <div id="live" class="max-w-screen bg-gray-100 m-auto flex flex-col justify-center items-center pb-14">
+    <h2 class="text-[25px] text-title_dark lg:text-3xl font-bold title pt-12 pb-12">
       Live Stream
     </h2>
 
@@ -12,14 +8,9 @@
     <!-- This Element has a custom css -->
     <div class="m-auto">
       <div class="flex justify-center">
-        <iframe
-          class=""
-          src="https://www.youtube.com/embed/Hh4QEcksgNQ"
-          title="YouTube video player"
-          frameborder="0"
+        <iframe class="" src="https://www.youtube.com/embed/Hh4QEcksgNQ" title="YouTube video player" frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-        ></iframe>
+          allowfullscreen></iframe>
       </div>
     </div>
     <!-- Youtube embeded Link end -->
